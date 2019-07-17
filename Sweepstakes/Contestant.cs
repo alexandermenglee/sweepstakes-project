27f2d8c658e326d84a2eadefb1da7015fe548bf5
Alexander Lee <alexandermlee10@gmail.com>
2019-07-17 10:22:02 -0500

    added Contestant class and a static UserInterface class

---
+++ Sweepstakes/Contestant.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sweepstakes
+{
+  public class Contestant
+  {
+    // Memeber variables
+    private string firstName;
+    private string lastName;
+    private string email;
+    private int registrationNumber;
+
+    public int RegistrationNumber
+    {
+      get => registrationNumber;
+    }
+
+    public string FirstName
+    {
+      set => firstName = value;
+    }
+
+    public string LastName
+    {
+      set => lastName = value;
+    }
+
+    public string Email
+    {
+      set => email = value;
+    }
+
+    // Constructor
+    public Contestant(string firstName, string lastName, string email)
+    {
+      this.firstName = firstName;
+      this.lastName = lastName;
+      this.email = email;
+    }
+  }
+}
---
+++ Sweepstakes/UserInterface.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sweepstakes
+{
+  public static class UserInterface
+  {
+    static UserInterface()
+    {
+
+    }
+    
+    public static string SetFirstName()
+    {
+      string firstName;
+
+      Console.WriteLine("First Name: ");
+      firstName = Console.ReadLine();
+      firstName.ToLower().Trim();
+
+      return firstName;
+    }
+
+    public static string SetLastName()
+    {
+      string lastName;
+
+      Console.WriteLine("Last Name: ");
+      lastName = Console.ReadLine();
+      lastName.ToLower().Trim();
+
+      return lastName;
+    }
+
+    public static string SetEmail()
+    {
+      string email;
+
+      Console.WriteLine("Email: ");
+      email = Console.ReadLine();
+      email.ToLower().Trim();
+
+      if (!email.Contains("@"))
+      {
+        Console.WriteLine("\nPlease enter in a valid email address");
+        SetEmail();
+      }
+
+      return email;
+    }
+
+    public static int SetRegistrationNumber()
+    {
+      string registrationNumber;
+
+
+      Console.WriteLine("\nManually set a contestants registration number: ");
+      registrationNumber = Console.ReadLine();
+
+      try
+      {
+        return int.Parse(registrationNumber);
+      }
+      catch(Exception e)
+      {
+        Console.WriteLine("\nEnter in numbers only");
+        return SetRegistrationNumber();
+      }
+    }
+  }
+}
